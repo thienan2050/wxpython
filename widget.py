943d680f9ee82a0bb43a06929663ddc946ff9a6f
thienan2050 <thienan2050@gmail.com>
2020-10-23 15:59:24 +0700

    Popup menu example

--- widget.py
+++ widget.py
@@ -1,105 +1,43 @@
-#!/usr/bin/env python
-
-"""
-ZetCode wxPython tutorial
-
-In this example, we create a submenu and a menu
-separator.
-
-author: Jan Bodnar
-website: www.zetcode.com
-last modified: July 2020
-"""
-
 import wx
 
+class MyFrame(wx.Frame):
+    def __init__(self):
+        wx.Frame.__init__(self, None, -1,"Popup Menu Example")
+        self.panel = p = wx.Panel(self)
+        menu = wx.Menu()
+        exit = menu.Append(-1, "Exit")
+        self.Bind(wx.EVT_MENU, self.OnExit, exit)
+                  
+        menuBar = wx.MenuBar()
+        menuBar.Append(menu, "Menu")
+        self.SetMenuBar(menuBar)
 
-class Example(wx.Frame):
+        wx.StaticText(p, -1,"Right-click on the panel to show a popup menu",(25,25))
 
-    def __init__(self, *args, **kwargs):
-        super(Example, self).__init__(*args, **kwargs)
-
-        self.InitUI()
-
-    def InitUI(self):
-        # Create MenuBar
-        menubar = wx.MenuBar()
-        
-        
-        # Create Menu 'File'
-        fileMenu = wx.Menu()
-        # Create items in Menu 'File'
-        newButton  = wx.MenuItem(fileMenu, wx.ID_NEW, '&New\tCtrl+N')
-        openButton = wx.MenuItem(fileMenu, wx.wxEVT_ANY, '&Open\tCtrl+O')
-        saveButton = wx.MenuItem(fileMenu, wx.wxEVT_ANY, '&Save\tCtrl+S')
-        quitButton = wx.MenuItem(fileMenu, wx.ID_EXIT, '&Quit\tCtrl+W')
-
-        fileMenu.AppendItem(newButton)
-        fileMenu.AppendItem(openButton)
-        fileMenu.AppendItem(saveButton)
-        fileMenu.AppendItem(quitButton)
-
-        # Create Menu 'Import'
-        imporT = wx.Menu()
-        # Create items in Menu 'Import'
-        NewFeed = wx.MenuItem(imporT, wx.wxEVT_ANY, '&NewFeed\tCtrl+F')
-        Bookmarks = wx.MenuItem(imporT, wx.wxEVT_ANY, '&Bookmarks\tCtrl+B')
-        imporT.AppendItem(NewFeed)
-        imporT.AppendItem(Bookmarks)
+        self.popupmenu = wx.Menu()
+        for text in "one two three four five".split():
+            item = self.popupmenu.Append(-1, text)
+            self.Bind(wx.EVT_MENU, self.OnPopupItemSelected, item)
+        p.Bind(wx.EVT_CONTEXT_MENU, self.OnShowPopup)
 
 
-        # Add Menu 'Import' to Menu 'File'
-        fileMenu.AppendMenu(wx.ID_ANY, '&Import', imporT)
-
-
-
-
+    def OnShowPopup(self, event):
+        pos = event.GetPosition()
+        pos = self.panel.ScreenToClient(pos)
+        self.panel.PopupMenu(self.popupmenu, pos)
         
 
+    def OnPopupItemSelected(self, event):
+        item = self.popupmenu.FindItemById(event.GetId())
+        text = item.GetText()
+        wx.MessageBox("You selected item '%s'" % text)
+        
 
-        self.Bind(wx.EVT_MENU, self.OnQuit, quitButton)
-        self.Bind(wx.EVT_MENU, self.OnOpen, openButton)
-        self.Bind(wx.EVT_MENU, self.OnSave, saveButton)
-        self.Bind(wx.EVT_MENU, self.OnNew, newButton)
+    def OnExit(self, event):
+        self.Close()
+        
 
-
-        # Add Menu 'Edit' to Menu 'File'
-        editMenu = wx.Menu()
-        # Create items in Menu 'Edit'
-        compileButton = wx.MenuItem(editMenu, wx.wxEVT_ANY, '&Compile\tCtrl+C')
-        editMenu.AppendItem(compileButton)
-
-
-
-        menubar.Append(fileMenu, '&File')
-        menubar.Append(editMenu, '&Edit')
-
-        #self.Bind(wx.EVT_MENU, self.OnCompile, )
-
-
-        self.SetMenuBar(menubar)
-
-        self.SetSize((350, 250))
-        self.SetTitle('OpenClovis IDE S7')
-        self.Centre()
-
-    def OnQuit(self, e):
-        self.Close()  
-    def OnSave(self, e):
-        print 'Saving'   
-    def OnOpen(self, e):
-        print 'Openning'         
-    def OnNew(self, e):
-        print 'New'
-
-
-def main():
-
-    app = wx.App()
-    ex = Example(None)
-    ex.Show()
-    app.MainLoop()
-
-
-if __name__ == '__main__':
-    main()
+app = wx.PySimpleApp()
+frame = MyFrame()
+frame.Show()
+app.MainLoop()
