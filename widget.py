d1b33748e37f8a064a82ade6d2a3c5097a3ea427
thienan2050 <thienan2050@gmail.com>
2020-10-27 17:00:33 +0700

    GridSizer

--- widget.py
+++ widget.py
@@ -4,8 +4,8 @@
 """
 ZetCode wxPython tutorial
 
-In this example we create a Go To class
-layout with wx.BoxSizer.
+In this example we create a layout
+of a calculator with wx.GridSizer.
 
 author: Jan Bodnar
 website: www.zetcode.com
@@ -13,6 +13,7 @@ last modified: July 2020
 """
 
 import wx
+
 
 class Example(wx.Frame):
 
@@ -22,77 +23,48 @@ class Example(wx.Frame):
         self.InitUI()
         self.Centre()
 
+
     def InitUI(self):
 
-        panel = wx.Panel(self)
-
-        font = wx.SystemSettings.GetFont(wx.SYS_SYSTEM_FONT)
-
-        font.SetPointSize(9)
+        menubar = wx.MenuBar()
+        fileMenu = wx.Menu()
+        menubar.Append(fileMenu, '&File')
+        self.SetMenuBar(menubar)
 
         vbox = wx.BoxSizer(wx.VERTICAL)
+        self.display = wx.TextCtrl(self, style=wx.TE_RIGHT)
+        vbox.Add(self.display, flag=wx.EXPAND|wx.TOP|wx.BOTTOM, border=30)
+        gs = wx.GridSizer(5, 4, 10, 10)
 
-        hbox1 = wx.BoxSizer(wx.HORIZONTAL)
-        st1 = wx.StaticText(panel, label='Class Name\t\t')
-        st1.SetFont(font)
-        hbox1.Add(st1, flag=wx.CENTER, border=20)
-        tc = wx.TextCtrl(panel)
-        hbox1.Add(tc, proportion=1)
-        vbox.Add(hbox1, flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
+        gs.AddMany( [(wx.Button(self, label='Cls'), 0, wx.EXPAND),
+            (wx.Button(self, label='Bck'), 0, wx.EXPAND),
+            (wx.StaticText(self), wx.EXPAND),
+            (wx.Button(self, label='Close'), 0, wx.EXPAND),
+            (wx.Button(self, label='7'), 0, wx.EXPAND),
+            (wx.Button(self, label='8'), 0, wx.EXPAND),
+            (wx.Button(self, label='9'), 0, wx.EXPAND),
+            (wx.Button(self, label='/'), 0, wx.EXPAND),
+            (wx.Button(self, label='4'), 0, wx.EXPAND),
+            (wx.Button(self, label='5'), 0, wx.EXPAND),
+            (wx.Button(self, label='6'), 0, wx.EXPAND),
+            (wx.Button(self, label='*'), 0, wx.EXPAND),
+            (wx.Button(self, label='1'), 0, wx.EXPAND),
+            (wx.Button(self, label='2'), 0, wx.EXPAND),
+            (wx.Button(self, label='3'), 0, wx.EXPAND),
+            (wx.Button(self, label='-'), 0, wx.EXPAND),
+            (wx.Button(self, label='0'), 0, wx.EXPAND),
+            (wx.Button(self, label='.'), 0, wx.EXPAND),
+            (wx.Button(self, label='='), 0, wx.EXPAND),
+            (wx.Button(self, label='+'), 0, wx.EXPAND) ])
 
-        vbox.Add((-1, 10))
-
-
-
-        hbox2 = wx.BoxSizer(wx.HORIZONTAL)
-        st2 = wx.StaticText(panel, label='Matching Classes')
-        st2.SetFont(font)
-        hbox2.Add(st2)
-        vbox.Add(hbox2, flag=wx.LEFT | wx.TOP | wx.RIGHT, border=10)
-
-        vbox.Add((-1, 10))
-
-        hbox3 = wx.BoxSizer(wx.HORIZONTAL)
-        tc2 = wx.TextCtrl(panel, style=wx.TE_MULTILINE)
-        hbox3.Add(tc2, proportion=1, flag=wx.EXPAND)
-        vbox.Add(hbox3, proportion=1, flag=wx.LEFT|wx.RIGHT|wx.EXPAND,
-            border=10)
-
-        vbox.Add((-1, 25))
-
-        hbox4 = wx.BoxSizer(wx.HORIZONTAL)
-        cb1 = wx.CheckBox(panel, label='Case Sensitive')
-        cb1.SetFont(font)
-        hbox4.Add(cb1)
-
-        cb2 = wx.CheckBox(panel, label='Nested Classes')
-        cb2.SetFont(font)
-        hbox4.Add(cb2, flag=wx.LEFT, border=10)
-        
-        cb3 = wx.CheckBox(panel, label='Non-Project classes')
-        cb3.SetFont(font)
-        hbox4.Add(cb3, flag=wx.LEFT, border=10)
-        
-        vbox.Add(hbox4, flag=wx.LEFT, border=10)
-
-        vbox.Add((-1, 25))
-
-        hbox5 = wx.BoxSizer(wx.HORIZONTAL)
-
-        btn1 = wx.Button(panel, label='Ok', size=(70, 30))
-        hbox5.Add(btn1)
-        
-        btn2 = wx.Button(panel, label='Close', size=(70, 30))
-        hbox5.Add(btn2, flag=wx.LEFT|wx.BOTTOM, border=5)
-        vbox.Add(hbox5, flag=wx.ALIGN_RIGHT|wx.RIGHT|wx.BOTTOM, border=20)
-
-        panel.SetSizer(vbox)
+        vbox.Add(gs, proportion=1, flag=wx.EXPAND)
+        self.SetSizer(vbox)
 
 
 def main():
 
     app = wx.App()
-    ex = Example(None, title='Go To Class')
+    ex = Example(None, title='Calculator')
     ex.Show()
     app.MainLoop()
 
