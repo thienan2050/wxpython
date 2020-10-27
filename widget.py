2e154643209ff53d9341707e995099b5b4c1eb57
thienan2050 <thienan2050@gmail.com>
2020-10-27 17:46:34 +0700

    wx.GridBagSizer

--- widget.py
+++ widget.py
@@ -3,8 +3,8 @@
 """
 ZetCode wxPython tutorial
 
-In this example we create review
-layout with wx.FlexGridSizer.
+In this example we create a rename layout
+with wx.GridBagSizer.
 
 author: Jan Bodnar
 website: www.zetcode.com
@@ -13,6 +13,7 @@ last modified: July 2020
 
 import wx
 
+
 class Example(wx.Frame):
 
     def __init__(self, parent, title):
@@ -20,38 +21,33 @@ class Example(wx.Frame):
 
         self.InitUI()
         self.Centre()
-        self.Show()
 
     def InitUI(self):
 
         panel = wx.Panel(self)
+        sizer = wx.GridBagSizer(4, 4)
 
-        hbox = wx.BoxSizer(wx.HORIZONTAL)
+        text = wx.StaticText(panel, label="Rename To")
+        sizer.Add(text, pos=(0, 0), flag=wx.TOP|wx.LEFT|wx.BOTTOM, border=5)
 
-        fgs = wx.FlexGridSizer(3, 2, 9, 25)
+        tc = wx.TextCtrl(panel)
+        sizer.Add(tc, pos=(1, 0), span=(1, 5),
+            flag=wx.EXPAND|wx.LEFT|wx.RIGHT, border=5)
 
-        title = wx.StaticText(panel, label="Title")
-        author = wx.StaticText(panel, label="Author")
-        review = wx.StaticText(panel, label="Review")
+        buttonOk = wx.Button(panel, label="Ok", size=(90, 28))
+        buttonClose = wx.Button(panel, label="Close", size=(90, 28))
+        sizer.Add(buttonOk, pos=(3, 3))
+        sizer.Add(buttonClose, pos=(3, 4), flag=wx.RIGHT|wx.BOTTOM, border=10)
 
-        tc1 = wx.TextCtrl(panel)
-        tc2 = wx.TextCtrl(panel)
-        tc3 = wx.TextCtrl(panel, style=wx.TE_MULTILINE)
-
-        fgs.AddMany([(title), (tc1, 1, wx.EXPAND), (author),
-            (tc2, 1, wx.EXPAND), (review, 1, wx.EXPAND), (tc3, 1, wx.EXPAND)])
-
-        fgs.AddGrowableRow(2, 10)
-        fgs.AddGrowableCol(1, 10)
-
-        hbox.Add(fgs, proportion=1, flag=wx.ALL|wx.EXPAND, border=15)
-        panel.SetSizer(hbox)
+        sizer.AddGrowableCol(1)
+        sizer.AddGrowableRow(2)
+        panel.SetSizer(sizer)
 
 
 def main():
 
     app = wx.App()
-    ex = Example(None, title='Review')
+    ex = Example(None, title='Rename')
     ex.Show()
     app.MainLoop()
 
