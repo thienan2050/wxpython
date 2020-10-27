b1c842068c2c60adb21ac8146caf6be11fd195a7
thienan2050 <thienan2050@gmail.com>
2020-10-27 16:23:15 +0700

    layout vertical box & horizontal box

--- widget.py
+++ widget.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
+
 """
 ZetCode wxPython tutorial
 
-In this example we place a panel inside 
-another panel.
+In this example we create a Go To class
+layout with wx.BoxSizer.
 
 author: Jan Bodnar
 website: www.zetcode.com
@@ -12,7 +13,6 @@ last modified: July 2020
 """
 
 import wx
-
 
 class Example(wx.Frame):
 
@@ -26,28 +26,76 @@ class Example(wx.Frame):
 
         panel = wx.Panel(self)
 
-        panel.SetBackgroundColour('#a539d1')
+        font = wx.SystemSettings.GetFont(wx.SYS_SYSTEM_FONT)
+
+        font.SetPointSize(9)
+
         vbox = wx.BoxSizer(wx.VERTICAL)
+
+        hbox1 = wx.BoxSizer(wx.HORIZONTAL)
+        st1 = wx.StaticText(panel, label='Class Name\t\t')
+        st1.SetFont(font)
+        hbox1.Add(st1, flag=wx.CENTER, border=20)
+        tc = wx.TextCtrl(panel)
+        hbox1.Add(tc, proportion=1)
+        vbox.Add(hbox1, flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
+
+        vbox.Add((-1, 10))
+
+
+
+        hbox2 = wx.BoxSizer(wx.HORIZONTAL)
+        st2 = wx.StaticText(panel, label='Matching Classes')
+        st2.SetFont(font)
+        hbox2.Add(st2)
+        vbox.Add(hbox2, flag=wx.LEFT | wx.TOP | wx.RIGHT, border=10)
+
+        vbox.Add((-1, 10))
+
+        hbox3 = wx.BoxSizer(wx.HORIZONTAL)
+        tc2 = wx.TextCtrl(panel, style=wx.TE_MULTILINE)
+        hbox3.Add(tc2, proportion=1, flag=wx.EXPAND)
+        vbox.Add(hbox3, proportion=1, flag=wx.LEFT|wx.RIGHT|wx.EXPAND,
+            border=10)
+
+        vbox.Add((-1, 25))
+
+        hbox4 = wx.BoxSizer(wx.HORIZONTAL)
+        cb1 = wx.CheckBox(panel, label='Case Sensitive')
+        cb1.SetFont(font)
+        hbox4.Add(cb1)
+
+        cb2 = wx.CheckBox(panel, label='Nested Classes')
+        cb2.SetFont(font)
+        hbox4.Add(cb2, flag=wx.LEFT, border=10)
         
+        cb3 = wx.CheckBox(panel, label='Non-Project classes')
+        cb3.SetFont(font)
+        hbox4.Add(cb3, flag=wx.LEFT, border=10)
+        
+        vbox.Add(hbox4, flag=wx.LEFT, border=10)
 
-        midPan = wx.Panel(panel)
-        midPan.SetBackgroundColour('#2a752e')
+        vbox.Add((-1, 25))
 
-        vbox.Add(midPan, wx.ID_ANY, wx.EXPAND | wx.ALL, 20)
+        hbox5 = wx.BoxSizer(wx.HORIZONTAL)
+
+        btn1 = wx.Button(panel, label='Ok', size=(70, 30))
+        hbox5.Add(btn1)
+        
+        btn2 = wx.Button(panel, label='Close', size=(70, 30))
+        hbox5.Add(btn2, flag=wx.LEFT|wx.BOTTOM, border=5)
+        vbox.Add(hbox5, flag=wx.ALIGN_RIGHT|wx.RIGHT|wx.BOTTOM, border=20)
+
         panel.SetSizer(vbox)
 
 
 def main():
 
     app = wx.App()
-    ex = Example(None, title='Border')
+    ex = Example(None, title='Go To Class')
     ex.Show()
     app.MainLoop()
 
 
 if __name__ == '__main__':
     main()
-
-
-
- 
