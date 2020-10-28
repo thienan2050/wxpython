91f3f71c53000b130813e4af897b72298a796d6c
thienan2050 <thienan2050@gmail.com>
2020-10-28 14:50:37 +0700

    Window identifiers

--- widget.py
+++ widget.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 
-import wx
-
 """
 ZetCode wxPython tutorial
 
-In this example we veto an event.
+In this example we use automatic ids
+with wx.ID_ANY.
 
 author: Jan Bodnar
 website: www.zetcode.com
 last modified: July 2020
 """
+
+import wx
+
 
 class Example(wx.Frame):
 
@@ -21,22 +23,31 @@ class Example(wx.Frame):
 
     def InitUI(self):
 
-        self.Bind(wx.EVT_CLOSE, self.OnCloseWindow)
+        panel = wx.Panel(self)
+        grid = wx.GridBagSizer(1, 1)
+        self.exitButton = wx.Button(panel, 100, 'Exit')
+        grid.Add(self.exitButton, pos=(0,0), flag=wx.TOP|wx.LEFT|wx.BOTTOM, border=30)
 
-        self.SetTitle('Event veto')
+
+        self.CGV = wx.Button(panel, 1342, 'CGV')
+        grid.Add(self.CGV, pos=(0,2), flag=wx.RIGHT|wx.TOP, border=30)
+
+
+
+        self.Bind(wx.EVT_BUTTON, self.Handler, id=self.exitButton.GetId())
+        self.Bind(wx.EVT_BUTTON, self.Handler, id=self.CGV.GetId())
+
+        self.SetTitle("Automatic ids")
         self.Centre()
 
-    def OnCloseWindow(self, e):
+        panel.SetSizer(grid)
+        grid.Fit(self)
 
-        dial = wx.MessageDialog(None, 'Are you sure to quit?', 'Question',
-            wx.YES_NO | wx.NO_DEFAULT | wx.ICON_QUESTION)
-
-        ret = dial.ShowModal()
-
-        if ret == wx.ID_YES:
-            self.Destroy()
-        else:
-            e.Veto()
+    def Handler(self, event):
+        if event.GetId() == self.exitButton.GetId():
+            self.Close()
+        elif event.GetId() == self.CGV.GetId():
+            print 'CGV button'
 
 
 def main():
