f14fd44dc93256489dec36362925d4ecb4028624
thienan2050 <thienan2050@gmail.com>
2020-10-28 14:18:29 +0700

    Promt on Exit

--- widget.py
+++ widget.py
@@ -1,123 +1,48 @@
 #!/usr/bin/env python
 
+import wx
+
 """
 ZetCode wxPython tutorial
 
-In this example we create a new class layout
-with wx.GridBagSizer.
+In this example we veto an event.
 
 author: Jan Bodnar
 website: www.zetcode.com
 last modified: July 2020
 """
 
-import wx
-
 class Example(wx.Frame):
 
-    def __init__(self, parent, title):
-        super(Example, self).__init__(parent, title=title)
+    def __init__(self, *args, **kw):
+        super(Example, self).__init__(*args, **kw)
 
         self.InitUI()
-        self.Centre()
 
     def InitUI(self):
 
-        panel = wx.Panel(self)
+        self.Bind(wx.EVT_CLOSE, self.OnCloseWindow)
 
-        # A grid 5 colums and 5 rows
-        sizer = wx.GridBagSizer(5, 5)
+        self.SetTitle('Event veto')
+        self.Centre()
 
-        text1 = wx.StaticText(panel, label="Java Class")
-        # row 0, colum 0.
-        sizer.Add(text1, pos=(0, 0), flag=wx.TOP|wx.LEFT|wx.BOTTOM,
-            border=15)
+    def OnCloseWindow(self, e):
 
-        # row 0, colum 4
-        icon = wx.StaticBitmap(panel, bitmap=wx.Bitmap('Trump.jpg'))
-        sizer.Add(icon, pos=(0, 4), flag=wx.TOP|wx.RIGHT|wx.ALIGN_RIGHT,
-            border=5)
+        dial = wx.MessageDialog(None, 'Are you sure to quit?', 'Question',
+            wx.YES_NO | wx.NO_DEFAULT | wx.ICON_QUESTION)
 
+        ret = dial.ShowModal()
 
-        line = wx.StaticLine(panel)
-        # row 1, colum 0
-        sizer.Add(line, pos=(1, 0), span=(1, 5),
-            flag=wx.EXPAND|wx.BOTTOM, border=10)
+        if ret == wx.ID_YES:
+            self.Destroy()
+        else:
+            e.Veto()
 
-        text2 = wx.StaticText(panel, label="Name")
-        # row 2, colum 0
-        sizer.Add(text2, pos=(2, 0), flag=wx.LEFT, border=10)
-
-        tc1 = wx.TextCtrl(panel)
-        # row 2, colum 1, expand from colum 1 to colum 3
-        sizer.Add(tc1, pos=(2, 1), span=(1, 3), flag=wx.TOP|wx.EXPAND)
-
-        text3 = wx.StaticText(panel, label="Package")
-        # row 3, colum 0
-        sizer.Add(text3, pos=(3, 0), flag=wx.LEFT|wx.TOP, border=10)
-
-        tc2 = wx.TextCtrl(panel)
-        # row 3, colum 1, expand from colum 1 to colum 3
-        sizer.Add(tc2, pos=(3, 1), span=(1, 3), flag=wx.TOP|wx.EXPAND,
-            border=5)
-
-        button1 = wx.Button(panel, label="Browse...")
-        # row 3, colum 4
-        sizer.Add(button1, pos=(3, 4), flag=wx.TOP|wx.RIGHT, border=5)
-
-        text4 = wx.StaticText(panel, label="Extends")
-        # row 4, colum 0
-        sizer.Add(text4, pos=(4, 0), flag=wx.TOP|wx.LEFT, border=10)
-
-        combo = wx.ComboBox(panel)
-        # row 4, colum 1, expand from colum 1 to colum 3
-        sizer.Add(combo, pos=(4, 1), span=(1, 3),
-            flag=wx.TOP|wx.EXPAND, border=5)
-
-        button2 = wx.Button(panel, label="Browse...")
-        # row 4, colum 4
-        sizer.Add(button2, pos=(4, 4), flag=wx.TOP|wx.RIGHT, border=5)
-
-        # Similar to StaticLine
-        sb = wx.StaticBox(panel, label="Optional Attributes")
-
-        # Similar to GridBagSizer
-        boxsizer = wx.StaticBoxSizer(sb, wx.VERTICAL)
-        boxsizer.Add(wx.CheckBox(panel, label="Public"),
-            flag=wx.LEFT|wx.TOP, border=5)
-        boxsizer.Add(wx.CheckBox(panel, label="Generate Default Constructor"),
-            flag=wx.LEFT, border=5)
-        boxsizer.Add(wx.CheckBox(panel, label="Generate Main Method"),
-            flag=wx.LEFT|wx.BOTTOM, border=5)
-        
-        
-        
-        sizer.Add(boxsizer, pos=(5, 0), span=(1, 5),
-            flag=wx.EXPAND|wx.TOP|wx.LEFT|wx.RIGHT , border=10)
-
-
-        button3 = wx.Button(panel, label='Help')
-        sizer.Add(button3, pos=(7, 0), flag=wx.LEFT, border=10)
-
-        button4 = wx.Button(panel, label="Ok")
-        sizer.Add(button4, pos=(7, 3))
-
-        button5 = wx.Button(panel, label="Cancel")
-        sizer.Add(button5, pos=(7, 4), span=(1, 1),
-            flag=wx.BOTTOM|wx.RIGHT, border=10)
-
-
-
-        sizer.AddGrowableCol(2)
-
-        panel.SetSizer(sizer)
-        sizer.Fit(self)
-        
 
 def main():
 
     app = wx.App()
-    ex = Example(None, title="Create Java Class")
+    ex = Example(None)
     ex.Show()
     app.MainLoop()
 
