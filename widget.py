6f6af5b8268deeb112e02cbbf74a88e301614984
thienan2050 <thienan2050@gmail.com>
2020-10-29 13:48:23 +0700

    Radio Button  + its event

--- widget.py
+++ widget.py
@@ -11,32 +11,38 @@ class Example(wx.Frame):
         self.InitUI()
         
     def InitUI(self):   
-
+            
         pnl = wx.Panel(self)
 
-        button = wx.Button(pnl, label='Button', pos=(20, 20))
-        text = wx.CheckBox(pnl, label='CheckBox', pos=(20, 90))
-        combo = wx.ComboBox(pnl, pos=(120, 22), choices=['Python', 'Ruby'])
-        slider = wx.Slider(pnl, 5, 6, 1, 10, (120, 90), (110, -1))        
+        self.rb1 = wx.RadioButton(pnl, label='Value A', pos=(10, 10), 
+            style=wx.RB_GROUP)
+        self.rb2 = wx.RadioButton(pnl, label='Value B', pos=(10, 30))
+        self.rb3 = wx.RadioButton(pnl, label='Value C', pos=(10, 50))
+        
+        self.rb1.Bind(wx.EVT_RADIOBUTTON, self.SetVal)
+        self.rb2.Bind(wx.EVT_RADIOBUTTON, self.SetVal)
+        self.rb3.Bind(wx.EVT_RADIOBUTTON, self.SetVal)
 
-        pnl.Bind(wx.EVT_ENTER_WINDOW, self.OnWidgetEnter)
-        button.Bind(wx.EVT_ENTER_WINDOW, self.OnWidgetEnter)
-        text.Bind(wx.EVT_ENTER_WINDOW, self.OnWidgetEnter)
-        combo.Bind(wx.EVT_ENTER_WINDOW, self.OnWidgetEnter)
-        slider.Bind(wx.EVT_ENTER_WINDOW, self.OnWidgetEnter)
+        self.sb = self.CreateStatusBar(3)
+        
+        self.sb.SetStatusText("True", 0)
+        self.sb.SetStatusText("False", 1)
+        self.sb.SetStatusText("False", 2)   
 
-        self.sb = self.CreateStatusBar()
-
-        self.SetSize((250, 230))
-        self.SetTitle('wx.Statusbar')
+        self.SetSize((210, 210))
+        self.SetTitle('wx.RadioButton')
         self.Centre()
         self.Show(True)     
 
-    def OnWidgetEnter(self, e):
+    def SetVal(self, e):
         
-        name = e.GetEventObject().GetClassName()
-        self.sb.SetStatusText(name + ' widget')
-        e.Skip()               
+        state1 = str(self.rb1.GetValue())
+        state2 = str(self.rb2.GetValue())
+        state3 = str(self.rb3.GetValue())
+
+        self.sb.SetStatusText(state1, 0)
+        self.sb.SetStatusText(state2, 1)
+        self.sb.SetStatusText(state3, 2)            
         
 def main():
     
