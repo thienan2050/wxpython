25296da00f5cde8212b7f5b5326b70b87b844945
thienan2050 <thienan2050@gmail.com>
2020-10-29 13:43:58 +0700

    Widget staticBox + its event

--- widget.py
+++ widget.py
@@ -1,82 +1,45 @@
-#!/usr/bin/env python
-
-"""
-ZetCode wxPython tutorial
-
-In this code example, we create a static line.
-
-author: Jan Bodnar
-website: www.zetcode.com
-last modified: July 2020
-"""
-
+#!/usr/bin/python
 
 import wx
 
 
 class Example(wx.Frame):
-
+           
     def __init__(self, *args, **kw):
-        super(Example, self).__init__(*args, **kw)
-
+        super(Example, self).__init__(*args, **kw) 
+        
         self.InitUI()
-
-    def InitUI(self):
+        
+    def InitUI(self):   
 
         pnl = wx.Panel(self)
-
-        font = wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.BOLD)
-        heading = wx.StaticText(self, label='The Central Europe',
-                                pos=(25, 15), size=(200, -1))
-        heading.SetFont(font)
-
-        wx.StaticLine(self, pos=(25, 50), size=(300,1))
-
-        wx.StaticText(self, label='Slovakia', pos=(25, 80))
-        wx.StaticText(self, label='Hungary', pos=(25, 100))
-        wx.StaticText(self, label='Poland', pos=(25, 120))
-        wx.StaticText(self, label='Czech Republic', pos=(25, 140))
-        wx.StaticText(self, label='Germany', pos=(25, 160))
-        wx.StaticText(self, label='Slovenia', pos=(25, 180))
-        wx.StaticText(self, label='Austria', pos=(25, 200))
-        wx.StaticText(self, label='Switzerland', pos=(25, 220))
-
-        wx.StaticText(self, label='5 445 000', pos=(250, 80))
-        wx.StaticText(self, label='10 014 000', pos=(250, 100))
-        wx.StaticText(self, label='38 186 000', pos=(250, 120))
-        wx.StaticText(self, label='10 562 000', pos=(250, 140))
-        wx.StaticText(self, label='81 799 000', pos=(250, 160))
-        wx.StaticText(self, label='2 050 000', pos=(250, 180))
-        wx.StaticText(self, label='8 414 000', pos=(250, 200))
-        wx.StaticText(self, label='7 866 000', pos=(250, 220))
-
-        wx.StaticLine(self, pos=(25, 260), size=(300,1))
-
-        tsum = wx.StaticText(self, label='164 336 000', pos=(240, 280))
-        sum_font = tsum.GetFont()
-        sum_font.SetWeight(wx.BOLD)
-        tsum.SetFont(sum_font)
-
-        btn = wx.Button(self, label='Close', pos=(140, 310))
+        
+        wx.StaticBox(pnl, label='Personal Info', pos=(5, 5), size=(240, 170))
+        wx.CheckBox(pnl, label='Male', pos=(15, 30))
+        wx.CheckBox(pnl, label='Married', pos=(15, 55))
+        wx.StaticText(pnl, label='Age', pos=(15, 95))
+        wx.SpinCtrl(pnl, value='1', pos=(55, 90), size=(60, -1), min=1, max=120)
+        
+        btn = wx.Button(pnl, label='Ok', pos=(90, 185), size=(60, -1))
 
         btn.Bind(wx.EVT_BUTTON, self.OnClose)
 
-        self.SetSize((360, 380))
-        self.SetTitle('wx.StaticLine')
+        self.SetSize((270, 250))
+        self.SetTitle('Static box')
         self.Centre()
-
+        self.Show(True)          
+        
     def OnClose(self, e):
-
-        self.Close(True)
-
-
+        
+        self.Close(True)    
+               
+        
 def main():
-
-    app = wx.App()
-    ex = Example(None)
-    ex.Show()
-    app.MainLoop()
+    
+    ex = wx.App()
+    Example(None)
+    ex.MainLoop()    
 
 
 if __name__ == '__main__':
-    main()
+    main()   
